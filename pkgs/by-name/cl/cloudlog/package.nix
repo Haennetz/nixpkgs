025afbae7fa9c9868dc435ed45be094ab9707f07
haennetz <johannes.kress@hotmail.de>
2025-12-11 09:16:07 +0100

    cloudlog: 2.7.6 -> 2.8.2

--- pkgs/by-name/cl/cloudlog/package.nix
+++ pkgs/by-name/cl/cloudlog/package.nix
@@ -9,13 +9,13 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "cloudlog";
-  version = "2.7.6";
+  version = "2.8.2";
 
   src = fetchFromGitHub {
     owner = "magicbug";
     repo = "Cloudlog";
     rev = version;
-    hash = "sha256-p9PmWRKvKGGZrexZJwYtb+LYto9npZ606QVo4pvDBak=";
+    hash = "sha256-+NE8gu7PxaA+CrDgkMEA/jtnBbOQt8WbvxY4BHOhQQI=";
   };
 
   postPatch = ''
